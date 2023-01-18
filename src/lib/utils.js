0b2eb075d10feba340f92a153255b571ae77d60b
Cláudio Pereira <git@claudiop.com>
2023-01-18 02:16:28 +0000

    Distance calculation functions

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -229,3 +229,22 @@ export function listDifferences(original, patch) {
 	}
 	return changes;
 }
+
+export function distance(lat1, lon1, lat2, lon2) {
+	var p = 0.017453292519943295; // Math.PI / 180
+	var c = Math.cos;
+	var a =
+		0.5 - c((lat2 - lat1) * p) / 2 + (c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))) / 2;
+
+	return 12742 * Math.asin(Math.sqrt(a)) * 1000; // 2 * R; R = 6371 km
+}
+
+export function getNearestStops(stopList, lat, lon, n = 30) {
+	stopList.sort((a, b) => {
+		let distA = distance(lat, lon, a.lat, a.lon);
+		let distB = distance(lat, lon, b.lat, b.lon);
+		return distA - distB;
+	});
+
+	return stopList.slice(0, n);
+}
