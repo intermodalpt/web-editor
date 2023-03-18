224656bd3b1246107bc2fed70ecc9ac1aec53c8d
Cláudio Pereira <git@claudiop.com>
2023-03-18 03:58:43 +0000

    Improve layout adjustment, navigation indicator

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,25 @@
 <script>
 	import Header from './Header.svelte';
 	import '../app.css';
+	import { navigating } from '$app/stores';
 </script>
 
-<div class="flex flex-col flex-1 gap-2 p2">
+{#if $navigating}
+	<div class="fixed inset-0 bg-white bg-opacity-50 z-50 flex items-center justify-center">
+		<div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900" />
+	</div>
+{/if}
+<div id="layout" class="min-h-[100%]">
 	<Header />
-	<main class="flex flex-col">
+	<main class="flex flex-col overflow-y-auto">
 		<slot />
 	</main>
 </div>
+
+<style>
+	#layout {
+		display: grid;
+		grid-template-rows: auto 1fr;
+		height: 100%;
+	}
+</style>
