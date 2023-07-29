62af3804d493e086d6386192b015e73e7264b7ac
Cláudio Pereira <git@claudiop.com>
2023-07-29 22:46:29 +0100

    Make the images editor tabbed and integrated with the uploader

--- src/routes/images/+page.js
+++ src/routes/images/+page.js
@@ -1,7 +1,4 @@
-import { dev, browser } from '$app/environment';
-import { get } from 'svelte/store';
-import { loadToken, stops, loadStops } from '$lib/stores.js';
-import { apiServer } from '$lib/settings';
+import { loadToken, } from '$lib/stores.js';
 
 export const csr = true;
 export const ssr = false;
@@ -9,26 +6,5 @@ export const prerender = false;
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ params, fetch }) {
-	if (!browser) {
-		return {
-			untagged: []
-		};
-	}
-
 	const token = await loadToken(fetch);
-
-	let untagged = await fetch(`${apiServer}/v1/stop_pics/dangling?p=0`, {
-		headers: {
-			authorization: `Bearer ${token}`
-		}
-	}).then((r) => r.json());
-
-	// Might be unneeded
-	if (get(stops) === undefined) {
-		await loadStops(fetch);
-	}
-
-	return {
-		untagged: untagged
-	};
 }
--- src/routes/images/+page.svelte
+++ src/routes/images/+page.svelte
@@ -1,29 +1,71 @@
 <script>
-	import ImageEditor from '$lib/editor/ImageEditor.svelte';
 	import { writable } from 'svelte/store';
+	import { Map, Marker } from 'maplibre-gl';
+	import 'maplibre-gl/dist/maplibre-gl.css';
+	import ImageEditor from '$lib/editor/ImageEditor.svelte';
+	import ImageUploader from '$lib/images/ImageUploader.svelte';
 	import { apiServer } from '$lib/settings.js';
 	import { token } from '$lib/stores.js';
 
-	/** @type {import('./$types').PageData} */
-	export let data;
+	const tabs = {
+		map: 0,
+		tagged: 1,
+		untagged: 2,
+		upload: 3
+	};
+
+	const tab = writable(tabs.map);
+	class Gallery {
+		constructor(asc = true, pageSize = defaultPageSize) {
+			this.pageSize = pageSize;
+			this.asc = asc;
+			this.pictures = [];
+			this.seenIds = new Set();
+		}
+
+		mergePictures(pictures) {
+			const newPictures = pictures.filter((pic) => {
+				return !this.seenIds.has(pic.id);
+			});
+
+			newPictures.forEach((pic) => {
+				this.seenIds.add(pic.id);
+			});
+
+			this.pictures = this.pictures
+				.concat(newPictures)
+				.sort((a, b) =>
+					this.asc
+						? (a.capture_date || a.upload_date).localeCompare(b.capture_date || b.upload_date)
+						: (b.capture_date || b.upload_date).localeCompare(a.capture_date || a.upload_date)
+				);
+		}
+
+		get nextPage() {
+			return this.pictures.length / this.pageSize;
+		}
+	}
+
+	let taggedGallery = new Gallery(false);
+	let untaggedGallery = new Gallery(false);
+
+	tab.subscribe((value) => {
+		if (value === tabs.tagged) {
+			if (taggedGallery.pictures.length === 0) {
+				loadMoreTaggedStops();
+			}
+		} else if (value === tabs.untagged) {
+			if (untaggedGallery.pictures.length === 0) {
+				loadMoreUntaggedStops();
+			}
+		}
+	});
 
 	let uploadModal = false;
 	let openImage = writable(null);
-	const pageSize = 20;
+	const defaultPageSize = 20;
 
-	// Map of all untagged stops
-	let untaggedStopPictures = Object.fromEntries(
-		data.untagged.map((stop) => {
-			return [stop.id, stop];
-		})
-	);
-
-	$: orderedUntaggedPictures = Object.values(untaggedStopPictures).sort((a, b) =>
-		(a.capture_date || a.upload_date).localeCompare(b.capture_date || b.upload_date)
-	);
-
-	function loadMoreUntaggedStops() {
-		let page = Object.keys(untaggedStopPictures).length / pageSize;
+	function pagesToFetch(page) {
 		let pages = [];
 		if (Math.floor(page) !== page) {
 			pages.push(Math.floor(page));
@@ -31,10 +73,15 @@
 		} else {
 			pages.push(Math.floor(page));
 		}
+		return pages;
+	}
+
+	function loadMoreTaggedStops() {
+		let pages = pagesToFetch(taggedGallery.nextPage);
 
 		Promise.all(
 			pages.map((page) => {
-				return fetch(`${apiServer}/v1/stop_pics/dangling?p=${page}`, {
+				return fetch(`${apiServer}/v1/stop_pics/latest?tagged_only=true&p=${page}`, {
 					headers: {
 						authorization: `Bearer ${$token}`
 					}
@@ -42,17 +89,43 @@
 			})
 		)
 			.then((pages) => {
+				let pictures = [];
 				pages.forEach((results) => {
-					results.forEach((image) => {
-						image.stops = [];
-					});
-					for (let image of results) {
-						untaggedStopPictures[image.id] = image;
-					}
-					untaggedStopPictures = untaggedStopPictures;
+					pictures = pictures.concat(results);
 				});
+				taggedGallery.mergePictures(pictures);
+				taggedGallery = taggedGallery;
 			})
-			.catch(() => alert('Unable to load untagged stops'));
+			.catch((e) => {
+				console.error(e);
+				alert('Unable to load tagged pictures: ' + e.message);
+			});
+	}
+
+	function loadMoreUntaggedStops() {
+		let pages = pagesToFetch(untaggedGallery.nextPage);
+
+		Promise.all(
+			pages.map((page) => {
+				return fetch(`${apiServer}/v1/stop_pics/latest?untagged_only=true&p=${page}`, {
+					headers: {
+						authorization: `Bearer ${$token}`
+					}
+				}).then((r) => r.json());
+			})
+		)
+			.then((pages) => {
+				let pictures = [];
+				pages.forEach((results) => {
+					pictures = pictures.concat(results);
+				});
+				untaggedGallery.mergePictures(pictures);
+				untaggedGallery = untaggedGallery;
+			})
+			.catch((e) => {
+				console.error(e);
+				alert('Unable to load tagged pictures: ' + e.message);
+			});
 	}
 
 	function openPic(id) {
@@ -71,42 +144,100 @@
 	<meta name="description" content="Catalogo de imagens" />
 </svelte:head>
 
-<div class="flex flex-col gap-4 w-full max-w-[900px] mt-4 self-center">
-	<div class="flex justify-end">
-		<a class="btn btn-primary" href="/images/upload">Enviar mais imagens</a>
+<div class="self-center w-11/12 my-4">
+	<div class="tabs ml-4">
+		<a
+			class="tab tab-lg tab-lifted"
+			class:tab-active={$tab === tabs.map}
+			on:click={() => {
+				$tab = tabs.map;
+			}}>Mapa</a
+		>
+		<a
+			class="tab tab-lg tab-lifted"
+			class:tab-active={$tab === tabs.tagged}
+			on:click={() => {
+				$tab = tabs.tagged;
+			}}>Catalogadas</a
+		>
+		<a
+			class="tab tab-lg tab-lifted"
+			class:tab-active={$tab === tabs.untagged}
+			on:click={() => {
+				$tab = tabs.untagged;
+			}}>Por catalogar</a
+		>
+		<a
+			class="tab tab-lg tab-lifted"
+			class:tab-active={$tab === tabs.upload}
+			on:click={() => {
+				$tab = tabs.upload;
+			}}>Enviar</a
+		>
 	</div>
 	<div class="card bg-base-100 shadow-xl">
-		<div class="card-body">
-			<h2 class="card-title">Por Catalogar</h2>
-			<div class="flex flex-col items-center">
-				{#if Object.keys(untaggedStopPictures).length === 0}
-					<span class="text-lg">Não há imagens por catalogar</span>
-				{/if}
-				<div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-					{#each orderedUntaggedPictures as pic}
-						<div class="p-2 flex justify-center items-center cursor-pointer">
-							<img
-								src={pic.url_medium}
-								class="rounded-box transition-all hover:scale-105"
-								on:click={() => {
-									openPic(pic.id);
-								}}
-							/>
-						</div>
-					{/each}
-				</div>
-				<div class="btn btn-primary w-full" on:click={() => loadMoreUntaggedStops()}>
-					Listar mais
+		{#if $tab === tabs.map}
+			<div class="card-body">
+
+			</div>
+		{:else if $tab === tabs.tagged}
+			<div class="card-body">
+				<h2 class="card-title">Catalogadas</h2>
+
+				<div class="flex flex-col items-center">
+					{#if taggedGallery.pictures.length === 0}
+						<span class="text-lg">Ainda não foram catalogadas imagens</span>
+					{/if}
+					<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-8">
+						{#each taggedGallery.pictures as pic}
+							<div class="p-2 flex justify-center items-center cursor-pointer">
+								<img
+									src={pic.url_medium}
+									class="rounded-box transition-all hover:scale-105"
+									on:click={() => {
+										openPic(pic.id);
+									}}
+								/>
+							</div>
+						{/each}
+					</div>
+					<div class="btn btn-secondary w-full" on:click={() => loadMoreTaggedStops()}>
+						Listar mais
+					</div>
 				</div>
 			</div>
-		</div>
-	</div>
+		{:else if $tab === tabs.untagged}
+			<div class="card-body">
+				<h2 class="card-title">Por Catalogar</h2>
 
-	<div class="card bg-base-100 shadow-xl self-center w-full">
-		<div class="card-body">
-			<h2 class="card-title">Catalogadas</h2>
-			<div class="flex flex-col items-center">Por fazer</div>
-		</div>
+				<div class="flex flex-col items-center">
+					{#if untaggedGallery.pictures.length === 0}
+						<span class="text-lg">Não há imagens por catalogar</span>
+					{/if}
+					<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-8">
+						{#each untaggedGallery.pictures as pic}
+							<div class="p-2 flex justify-center items-center cursor-pointer">
+								<img
+									src={pic.url_medium}
+									class="rounded-box transition-all hover:scale-105"
+									on:click={() => {
+										openPic(pic.id);
+									}}
+								/>
+							</div>
+						{/each}
+					</div>
+					<div class="btn btn-secondary w-full" on:click={() => loadMoreUntaggedStops()}>
+						Listar mais
+					</div>
+				</div>
+			</div>
+		{:else if $tab === tabs.upload}
+			<div class="card-body">
+				<h2 class="card-title">Enviar imagens</h2>
+				<ImageUploader />
+			</div>
+		{/if}
 	</div>
 </div>
 
