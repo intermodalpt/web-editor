6cf760dcc0f0611c56e1b5cd0c8537bf8e6dc6ac
Cláudio Pereira <git@claudiop.com>
2023-08-01 04:46:57 +0100

    Distance threshold setting that was left uncommitted a few commits ago by mistake.

--- src/lib/settings.js
+++ src/lib/settings.js
@@ -7,3 +7,7 @@ export const tileStyle = 'https://tiles2.intermodal.pt/styles/iml/style.json';
 const minute = 1000 * 60;
 export const cacheRefreshTime = minute * 60; // 1 hour
 export const cacheInvalidationTime = minute * 60 * 6; // 6 hours
+
+
+// About 1cm at Portugal's latitude
+export const movementTreshold = 0.000001;
