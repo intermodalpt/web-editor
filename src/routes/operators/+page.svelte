5d567ff7b1ce4fdadee9979f2ec196293984624e
Cláudio Pereira <git@claudiop.com>
2024-03-10 23:57:58 +0000

    List outside-region operators in the operators page

--- src/routes/operators/+page.svelte
+++ src/routes/operators/+page.svelte
@@ -5,18 +5,24 @@
 
 	const operators = liveQuery(() => getOperators());
 
-	const regionOperators = derived([operators, regionId], ([$operators, $regionId]) => {
-		if (!$operators || !$regionId) {
-			return [];
-		}
-		return Object.values($operators).filter((operator) => {
+	const sortedOperators = derived(operators, ($operators) => {
+		if (!$operators) return [];
+		return Object.values($operators).sort((a, b) => {
+			return a.name.localeCompare(b.name);
+		});
+	});
+
+	const regionOperators = derived([sortedOperators, regionId], ([$sortedOperators, $regionId]) => {
+		if (!$regionId || !$sortedOperators) return [];
+		return $sortedOperators.filter((operator) => {
 			return operator.regions.includes($regionId);
 		});
 	});
 
-	const sortedOperators = derived(regionOperators, ($regionOperators) => {
-		return Object.entries($regionOperators || []).sort(([, a], [, b]) => {
-			return a.name.localeCompare(b.name);
+	const otherOperators = derived([sortedOperators, regionId], ([$sortedOperators, $regionId]) => {
+		if (!$regionId || !$sortedOperators) return [];
+		return $sortedOperators.filter((operator) => {
+			return !operator.regions.includes($regionId);
 		});
 	});
 
@@ -35,7 +41,21 @@
 	<div class="card-body">
 		<h2 class="card-title">Operadores</h2>
 		<div class="grid p-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3">
-			{#each Object.values($sortedOperators || {}) as [id, operator]}
+			{#each $regionOperators as operator}
+				<a
+					class="border-[2px] pointer-cursor rounded-lg hover:bg-base-200 p-1 flex items-start"
+					href="/operators/{operator.id}-{operator.tag}"
+				>
+					{#if operator.logo_url}
+						<img class="h-12" src={operator.logo_url} alt={operator.name} />
+					{/if}
+					<span class="text-lg lg:text-xl font-bold p-3">{operator.name}</span>
+				</a>
+			{/each}
+		</div>
+		<div class="divider">Outros</div>
+		<div class="grid p-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3">
+			{#each $otherOperators as operator}
 				<a
 					class="border-[2px] pointer-cursor rounded-lg hover:bg-base-200 p-1 flex items-start"
 					href="/operators/{operator.id}-{operator.tag}"
