bae211e035ab760c5e2fa607f8fcb4744c4fdd24
Cláudio Pereira <git@claudiop.com>
2024-05-07 22:24:06 +0100

    Add listing support for validated-external and internal news

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -7,6 +7,7 @@
 	import { decodedToken } from '$lib/stores.js';
 	import { getOperators, fetchOperators, getRegions, fetchRegions, loadMissing } from '$lib/db';
 	import Paginator from '$lib/components/Paginator.svelte';
+	import NewsItem from './NewsItem.svelte';
 	import ExternalNewsItem from './ExternalNewsItem.svelte';
 	import ExternalNewsItemEditor from './ExternalNewsItemEditor.svelte';
 
@@ -15,37 +16,38 @@
 	const regions = liveQuery(() => getRegions());
 
 	const mapTabs = {
-		pending: 0
+		pendingExt: 0,
+		allExt: 1,
+		allInt: 2
 	};
 
-	const tab = writable(mapTabs.pending);
+	const tab = writable(mapTabs.pendingExt);
 	let editDialog;
 	let editItemId;
 
-	const openContribution = writable(null);
-	const openChangeset = writable(null);
-
 	const operatorFilter = writable(null);
 
-	let contributors = [];
+	let pendingExternalLoaded = false;
+	let allExternalLoaded = false;
+	let allInternalLoaded = false;
 
-	let pendingLoaded = false;
-	let operatorsLoaded = false;
-
-	const pendingPage = writable(0);
+	const pendingExternalPage = writable(0);
+	const allExternalPage = writable(0);
+	const allInternalPage = writable(0);
 
 	// Singin', "This'll be the day that I die
 	// This'll be the day that I die,"
 	// -
 	const forceDerivedStoresToUpdate = writable(0);
 
-	let pendingTotal = 0;
+	let pendingExternalTotal = 0;
+	let allExternalTotal = 0;
+	let allInternalTotal = 0;
 
-	const pendingNews = derived(
-		[pendingPage, operatorFilter, forceDerivedStoresToUpdate],
+	const pendingExternalNews = derived(
+		[pendingExternalPage, operatorFilter, forceDerivedStoresToUpdate],
 		async ([$page, $OperatorFilter], set) => {
-			pendingLoaded = false;
-			console.log('fetching pending external', $page);
+			pendingExternalLoaded = false;
 			const fetchParams = {
 				headers: {
 					authorization: `Bearer ${$token}`
@@ -53,12 +55,58 @@
 			};
 
 			const res = await ($OperatorFilter
-				? fetch(`${apiServer}/v1/operators/${$operatorFilter}/external_news/pending`, fetchParams)
+				? fetch(
+						`${apiServer}/v1/operators/${$operatorFilter}/external_news/pending?p=${$page}`,
+						fetchParams
+					)
+				: fetch(`${apiServer}/v1/news/external/pending?p=${$page}`, fetchParams));
+			const data = await res.json();
+			pendingExternalTotal = data.total;
+			set(data.items);
+			pendingExternalLoaded = true;
+		}
+	);
+
+	const allExternalNews = derived(
+		[pendingExternalPage, operatorFilter, forceDerivedStoresToUpdate],
+		async ([$page, $OperatorFilter], set) => {
+			allExternalLoaded = false;
+			const fetchParams = {
+				headers: {
+					authorization: `Bearer ${$token}`
+				}
+			};
+
+			const res = await ($OperatorFilter
+				? fetch(
+						`${apiServer}/v1/operators/${$operatorFilter}/external_news?p=${$page}`,
+						fetchParams
+					)
 				: fetch(`${apiServer}/v1/news/external?p=${$page}`, fetchParams));
 			const data = await res.json();
-			pendingTotal = data.total;
+			allExternalTotal = data.total;
 			set(data.items);
-			pendingLoaded = true;
+			allExternalLoaded = true;
+		}
+	);
+
+	const allInternalNews = derived(
+		[allInternalPage, operatorFilter, forceDerivedStoresToUpdate],
+		async ([$page, $OperatorFilter], set) => {
+			allInternalLoaded = false;
+			const fetchParams = {
+				headers: {
+					authorization: `Bearer ${$token}`
+				}
+			};
+
+			const res = await ($OperatorFilter
+				? fetch(`${apiServer}/v1/operators/${$operatorFilter}/news?p=${$page}`, fetchParams)
+				: fetch(`${apiServer}/v1/news?p=${$page}`, fetchParams));
+			const data = await res.json();
+			allInternalTotal = data.total;
+			set(data.items);
+			allInternalLoaded = true;
 		}
 	);
 
@@ -66,7 +114,9 @@
 		console.log('refreshing data');
 		// Force a data refresh
 		// This should work...
-		$pendingPage = $pendingPage;
+		$pendingExternalPage = $pendingExternalPage;
+		$allExternalPage = $allExternalPage;
+		$allInternalPage = $allInternalPage;
 		// It doesn't. Let's drown a kitten
 		$forceDerivedStoresToUpdate = $forceDerivedStoresToUpdate + 1;
 	}
@@ -82,30 +132,44 @@
 </script>
 
 <svelte:head>
-	<title>Noticias externas</title>
-	<meta name="description" content="Noticias externas" />
+	<title>Noticias</title>
+	<meta name="description" content="Noticias" />
 </svelte:head>
 
 <div class="self-center max-w-[80em] w-full my-4">
 	<div class="tabs ml-4">
 		<button
 			class="tab tab-md lg:tab-lg tab-lifted"
-			class:tab-active={$tab === mapTabs.pending}
+			class:tab-active={$tab === mapTabs.pendingExt}
 			on:click={() => {
-				$tab = mapTabs.pending;
-			}}>Pendentes</button
+				$tab = mapTabs.pendingExt;
+			}}>Externas pendentes</button
+		>
+		<button
+			class="tab tab-md lg:tab-lg tab-lifted"
+			class:tab-active={$tab === mapTabs.allExt}
+			on:click={() => {
+				$tab = mapTabs.allExt;
+			}}>Externas</button
+		>
+		<button
+			class="tab tab-md lg:tab-lg tab-lifted"
+			class:tab-active={$tab === mapTabs.allInt}
+			on:click={() => {
+				$tab = mapTabs.allInt;
+			}}>Internas</button
 		>
 	</div>
 	<div class="card card-compact 2xl:card-normal bg-base-100 shadow-sm self-start">
 		<div class="card-body">
-			{#if $tab === mapTabs.pending}
+			{#if $tab === mapTabs.pendingExt}
 				<div class="flex flex-wrap gap-2 justify-between">
 					<h2 class="card-title">Conteúdos pendentes</h2>
 					<Paginator
-						bind:page={$pendingPage}
-						bind:itemCount={pendingTotal}
+						bind:page={$pendingExternalPage}
+						bind:itemCount={pendingExternalTotal}
 						on:goto={(e) => {
-							$pendingPage = e.detail.page;
+							$pendingExternalPage = e.detail.page;
 						}}
 					/>
 					{#if $operators}
@@ -121,9 +185,9 @@
 						</div>
 					{/if}
 				</div>
-				{#if pendingLoaded}
+				{#if pendingExternalLoaded}
 					<div class="flex flex-col gap-2">
-						{#each $pendingNews as item (item.id)}
+						{#each $pendingExternalNews as item (item.id)}
 							<button
 								class="p-2 border-2 rounded-lg cursor-pointer bg-base-100 hover:bg-base-200"
 								on:click={async () => {
@@ -132,7 +196,93 @@
 									editDialog.showModal();
 								}}
 							>
-								<ExternalNewsItem {item} />
+								<ExternalNewsItem {item} {operators} {regions} />
+							</button>
+						{/each}
+					</div>
+				{:else}
+					<div class="w-full flex justify-center">
+						<span class="loading loading-dots loading-lg" />
+					</div>
+				{/if}
+			{:else if $tab === mapTabs.allExt}
+				<div class="flex flex-wrap gap-2 justify-between">
+					<h2 class="card-title">Noticias externas</h2>
+					<Paginator
+						bind:page={$allExternalPage}
+						bind:itemCount={allExternalTotal}
+						on:goto={(e) => {
+							$allExternalPage = e.detail.page;
+						}}
+					/>
+					{#if $operators}
+						<div class="input-group w-fit">
+							<span class="bg-base-200 label-text">Filtros</span>
+							<span class="label-text">Operador</span>
+							<select bind:value={$operatorFilter} class="input h-full input-bordered">
+								<option selected value>-------</option>
+								{#each Object.values($operators ?? {}) as operator}
+									<option value={operator.id}>{operator.name}</option>
+								{/each}
+							</select>
+						</div>
+					{/if}
+				</div>
+				{#if allExternalLoaded}
+					<div class="flex flex-col gap-2">
+						{#each $allExternalNews as item (item.id)}
+							<button
+								class="p-2 border-2 rounded-lg cursor-pointer bg-base-100 hover:bg-base-200"
+								on:click={async () => {
+									editItemId = item.id;
+									await tick();
+									editDialog.showModal();
+								}}
+							>
+								<ExternalNewsItem {item} {operators} {regions} />
+							</button>
+						{/each}
+					</div>
+				{:else}
+					<div class="w-full flex justify-center">
+						<span class="loading loading-dots loading-lg" />
+					</div>
+				{/if}
+			{:else if $tab === mapTabs.allInt}
+				<div class="flex flex-wrap gap-2 justify-between">
+					<h2 class="card-title">Noticias internas</h2>
+					<Paginator
+						bind:page={$allInternalPage}
+						bind:itemCount={allInternalTotal}
+						on:goto={(e) => {
+							$allInternalPage = e.detail.page;
+						}}
+					/>
+					{#if $operators}
+						<div class="input-group w-fit">
+							<span class="bg-base-200 label-text">Filtros</span>
+							<span class="label-text">Operador</span>
+							<select bind:value={$operatorFilter} class="input h-full input-bordered">
+								<option selected value>-------</option>
+								{#each Object.values($operators ?? {}) as operator}
+									<option value={operator.id}>{operator.name}</option>
+								{/each}
+							</select>
+						</div>
+					{/if}
+				</div>
+				{#if allInternalLoaded}
+					<div class="flex flex-col gap-2">
+						{#each $allInternalNews as item (item.id)}
+							<button
+								class="p-2 border-2 rounded-lg cursor-pointer bg-base-100 hover:bg-base-200"
+								on:click={async () => {
+									editItemId = item.id;
+									await tick();
+									editDialog.showModal();
+								}}
+							>
+								<NewsItem {item} {operators} {regions} />
 							</button>
 						{/each}
 					</div>
---
+++ src/routes/news/NewsItem.svelte
@@ -0,0 +1,42 @@
+<script>
+	import BooleanToggle from '$lib/components/BooleanToggle.svelte';
+
+	export let item;
+	export let operators;
+	export let regions;
+</script>
+
+<h2 class="card-title text-lg">
+	<span class="text-xs">#{item.id}</span>
+	{item.title}
+</h2>
+<p class="text-justify">{item.summary}</p>
+
+<div class="flex gap-2 flex-wrap justify-between">
+	<div class="flex gap-2 flex-wrap">
+		{#if $operators}
+			{#each item.operator_ids as operator_id}
+				<span class="badge badge-outline border-orange-500">{$operators[operator_id]?.name}</span>
+			{/each}
+		{/if}
+		{#if $regions}
+			{#each item.region_ids as region_id}
+				<span class="badge badge-outline border-green-500">{$regions[region_id]?.name}</span>
+			{/each}
+		{/if}
+	</div>
+</div>
+
+<div class="flex gap-2 flex-wrap justify-between">
+	<div class="flex gap-2 flex-wrap" />
+
+	<div class="flex flex-wrap gap-2 justify-end">
+		<span class="label-text">Pub:{new Date(item.publish_datetime).toLocaleString('pt')}</span>
+		<span class="label-text">Edit:{new Date(item.edit_datetime).toLocaleString('pt')}</span>
+
+		<div class="flex gap-2 flex-wrap">
+			<span>Visível:</span>
+			<BooleanToggle state={item.is_visible} disabled={true} compact={true} nullable={false} />
+		</div>
+	</div>
+</div>
