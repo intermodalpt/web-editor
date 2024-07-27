5044143b5c8e124fca967cac53d946d84e6659b3
Cláudio Pereira <git@claudiop.com>
2024-07-27 12:35:51 +0100

    Have 'admin' as a universal do-it-all permission

--- src/lib/permissions.ts
+++ src/lib/permissions.ts
@@ -3,41 +3,41 @@ export function isAdmin(permissions: [{ perm: string }]) {
 }
 
 export function canCreateStops(permissions: [{ perm: string }]) {
-	return permissions.some((p) => p.perm === 'stops' || p.create);
+	return permissions.some((p) => p.perm === 'admin' || p.perm === 'stops' && p.create);
 }
 
 export function canMoveStops(permissions: [{ perm: string }]) {
-	return permissions.some((p) => p.perm === 'stops' || p.modify_pos);
+	return permissions.some((p) => p.perm === 'admin' || p.perm === 'stops' && p.modify_pos);
 }
 
 export function canModifyStopAttrs(permissions: [{ perm: string }]) {
-	return permissions.some((p) => p.perm === 'stops' || p.modify_attrs);
+	return permissions.some((p) => p.perm === 'admin' || p.perm === 'stops' && p.modify_attrs);
 }
 
 export function canContribModifyStopAttrs(permissions: [{ perm: string }]) {
-	return permissions.some((p) => p.perm === 'stops' || p.contrib_modify_attrs);
+	return permissions.some((p) => p.perm === 'admin' || p.perm === 'stops' && p.contrib_modify_attrs);
 }
 
 export function canCreateRoutes(permissions: [{ perm: string }]) {
-	return permissions.some((p) => p.perm === 'routes' || p.create);
+	return permissions.some((p) => p.perm === 'admin' || p.perm === 'routes' && p.create);
 }
 
 export function canCreateNews(permissions: [{ perm: string }]) {
-	return permissions.some((p) => p.perm === 'news' || p.create);
+	return permissions.some((p) => p.perm === 'admin' || p.perm === 'news' && p.create);
 }
 
 export function canModifyNews(permissions: [{ perm: string }]) {
-	return permissions.some((p) => p.perm === 'news' || p.modify);
+	return permissions.some((p) => p.perm === 'admin' || p.perm === 'news' && p.modify);
 }
 
 export function canDeleteNews(permissions: [{ perm: string }]) {
-	return permissions.some((p) => p.perm === 'routes' || p.delete);
+	return permissions.some((p) => p.perm === 'admin' || p.perm === 'routes' && p.delete);
 }
 
 export function canUploadPics(permissions: [{ perm: string }]) {
-	return permissions.some((p) => p.perm === 'stopPics' || p.upload);
+	return permissions.some((p) => p.perm === 'admin' || p.perm === 'stopPics' && p.upload);
 }
 
 export function canContribUploadPics(permissions: [{ perm: string }]) {
-	return permissions.some((p) => p.perm === 'stopPics' || p.contrib_upload);
+	return permissions.some((p) => p.perm === 'admin' || p.perm === 'stopPics' && p.contrib_upload);
 }
