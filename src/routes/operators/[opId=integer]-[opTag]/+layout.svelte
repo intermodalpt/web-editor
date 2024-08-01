38cc02de8005d9c12b221915c1c723c97f05b54c
Cláudio Pereira <git@claudiop.com>
2024-08-01 04:20:57 +0100

    Make the operator hierarchy a bit narrower in big screens

--- src/routes/operators/[opId=integer]-[opTag]/+layout.svelte
+++ src/routes/operators/[opId=integer]-[opTag]/+layout.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-col max-w-[100em] w-full sm:px-4 self-center my-4">
+<div class="flex flex-col w-[min(80em,100%)] sm:px-4 self-center my-4">
 	<div class="card rounded-md bg-base-100 shadow-sm">
 		<slot />
 	</div>
