4bc8f7ab4aed295d41e5d02ae96dad2551f6f9c7
Cláudio Pereira <git@claudiop.com>
2024-08-01 04:16:00 +0100

    Add stroke property to Icon

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -13,6 +13,7 @@
 		{
 			box: [24, 24],
 			name: 'info',
+			stroke: 'currentColor',
 			svg: `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>`
 		},
 		{
@@ -107,6 +108,7 @@
 	class={$$props.class}
 	{focusable}
 	fill={displayIcon.fill || 'none'}
+	stroke={displayIcon.stroke || 'none'}
 	viewBox="0 0 {displayIcon.box[0]} {displayIcon.box[1]}"
 >
 	{@html displayIcon.svg}
