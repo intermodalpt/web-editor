0b12c1091e56677ecb3ac0e0686dbb631b1c7973
Cláudio Pereira <git@claudiop.com>
2024-08-04 03:47:13 +0100

    Update DaisyUI to V4

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,12 +18,13 @@ module.exports = {
 		themes: [
 			{
 				winter: {
-					...require('daisyui/src/theming/themes')['[data-theme=winter]'],
+					...require('daisyui/src/theming/themes')["winter"],
 					secondary: '#60a5fa',
 					success: '#22c55e',
 					warning: '#fcd34d',
 					info: '#3abff8',
-					error: '#f87171'
+					error: '#f87171',
+					neutral: "#6b7280",
 				}
 			}
 		]
